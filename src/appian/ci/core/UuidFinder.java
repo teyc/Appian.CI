383160307c6b3ec31c21016cca6a2f8c6d28fc8c
Chui Tey <chui.tey@readify.net>
2016-01-14 17:19:50 +1000

    Find uuid in attributes as well

--- src/appian/ci/core/UuidFinder.java
+++ src/appian/ci/core/UuidFinder.java
@@ -2,14 +2,15 @@ package appian.ci.core;
 
 import java.util.LinkedList;
 import java.util.List;
-import org.xml.sax.AttributeList;
-import org.xml.sax.HandlerBase;
+import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
 
-public class UuidFinder extends HandlerBase {
+public class UuidFinder extends DefaultHandler {
     
     private final UuidUtil uuidUtil;
-
+    private final String uuidMatch = ".*(?i)(Uuid)";
+    
     public UuidFinder() {
         uuids = new LinkedList<>();
         uuidUtil = new UuidUtil();
@@ -20,25 +21,33 @@ public class UuidFinder extends HandlerBase {
     String currentNodeName;
 
     @Override
-    public void startElement(String nodeName, AttributeList al) throws SAXException {
-
-        currentNodeName = nodeName;
+    public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+        currentNodeName = qName;
         textNodeAccumulator = "";
-
+     
+        for (int i = 0; i < attributes.getLength(); i++) {
+            String attributeQName = attributes.getQName(i);
+            String attributeValue = attributes.getValue(i);
+            boolean isUuidAttribute = attributeQName.matches(uuidMatch);
+            if (isUuidAttribute && uuidUtil.fromString(attributeValue) != null) {
+                uuids.add(attributeValue);
+            }
+        }
     }
 
+    @Override
     public void characters(char[] chars, int start, int length) throws SAXException {
         String contents = new String(chars, start, length);
         textNodeAccumulator += contents;
     }
 
-    public void endElement(String nodeName) throws SAXException {
-
-        boolean isUuidNode = nodeName.matches(".*(?i)(Uuid)");
+    @Override
+    public void endElement(String uri, String localName, String qName) throws SAXException {
+    
+        boolean isUuidNode = qName.matches(uuidMatch);
 
         if (isUuidNode && uuidUtil.fromString(textNodeAccumulator) != null) {
             uuids.add(textNodeAccumulator);
-            isUuidNode = false;
         }
 
         textNodeAccumulator = "";
