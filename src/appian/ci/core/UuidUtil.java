a6d5097db63de036ccf503bc29e6f751dd22d877
Chui Tey <chui.tey@readify.net>
2016-01-18 14:33:57 +1000

    exclude -g_<uuid> because processModels uuids are not supported by Appian. This is done in QueryName

--- src/appian/ci/core/UuidUtil.java
+++ src/appian/ci/core/UuidUtil.java
@@ -1,22 +1,26 @@
 package appian.ci.core;
 
-import java.util.UUID;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UuidUtil {
     
     // matches any of these
+    // 0000dc11-edca-8000-f92f-7f0000014e7a  
+    // -a_0000dc11-edca-8000-f92f-7f0000014e7a  
+    // _c_0000dc11-edca-8000-f92f-7f0000014e7a  
+    //
+    // Will not match this - as it's a processModelFolder, and the UUID is not 
+    // recognized by getcontentdetailbyuuid or getprocessmodelbyuuid
     // _g-0000dc11-edca-8000-f92f-7f0000014e7a_74
-    // 0000dc11-edca-8000-f92f-7f0000014e7a       
-    Pattern regex = Pattern.compile("(_.-)?(\\p{XDigit}{8}-\\p{XDigit}{4}-\\p{XDigit}{4}-\\p{XDigit}{4}-\\p{XDigit}{12})(_\\d+)?");
+    Pattern regex = Pattern.compile("((_[a-fh-z]-)?(\\p{XDigit}{8}-\\p{XDigit}{4}-\\p{XDigit}{4}-\\p{XDigit}{4}-\\p{XDigit}{12})(_\\d+)?)");
                
-    public UUID fromString(String uuidString)
+    public String fromString(String uuidString)
     {
         Matcher matcher = regex.matcher(uuidString);
         if (matcher.matches())
         {
-            return UUID.fromString(matcher.group(2));
+            return matcher.group(1);
         }
         
         return null;
--- test/appian/ci/resources/UuidUtilTest.java
+++ test/appian/ci/resources/UuidUtilTest.java
@@ -21,20 +21,31 @@ public class UuidUtilTest {
     }
     
     @Test
-    public void parsedUuids()
+    public void processModelFoldersAreNotUuids()
     {   
-        Assert.assertEquals(
-            "0000dc11-edca-8000-f92f-7f0000014e7a", 
-            util.fromString("_g-0000dc11-edca-8000-f92f-7f0000014e7a_74").toString());
+        /* This is an Appian bug */
+        Assert.assertNull(
+            util.fromString("_g-0000dc11-edca-8000-f92f-7f0000014e7a_74"));
+    }
+
+    @Test
+    public void applicationsAreUuids()
+    {   
+        Assert.assertEquals("_a-0000dc11-edca-8000-f92f-7f0000014e7a",
+            util.fromString("_a-0000dc11-edca-8000-f92f-7f0000014e7a"));
+    }
+
+    @Test
+    public void nonHexNumbersAreNotUuids()
+    {
+        Assert.assertNull(util.fromString("H0388428-5429-4df4-a72b-3272ffa3ce8e"));
+      
     }
 
     @Test
     public void uuidsAreDetected()
     {   
-        Assert.assertNotNull(util.fromString("_g-0000dc11-edca-8000-f92f-7f0000014e7a_74"));
-        Assert.assertNotNull(util.fromString("60388428-5429-4df4-a72b-3272ffa3ce8e"));
-        
-        Assert.assertNull(util.fromString("H0388428-5429-4df4-a72b-3272ffa3ce8e"));
+        Assert.assertEquals("60388428-5429-4df4-a72b-3272ffa3ce8e", util.fromString("60388428-5429-4df4-a72b-3272ffa3ce8e"));        
     }
     
 }
