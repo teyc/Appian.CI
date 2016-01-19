b1d5181285a4e2b36ec02d36a2c3462e57a84ae1
Chui Tey <chui.tey@readify.net>
2016-01-20 08:58:50 +1000

    Fix getResourceAsStream path

--- src/common/HelpPrinter.java
+++ src/common/HelpPrinter.java
@@ -10,7 +10,7 @@ import java.util.logging.Logger;
 public class HelpPrinter {
 
     public void printHelp(String command, OutputStream outputStream) {
-        String path = String.format("appian.ci.%s.README.md", command.toLowerCase());
+        String path = String.format("appian/ci/%s/README.md", command.toLowerCase());
         try (InputStream inputStream = AppianCI.class.getClassLoader().getResourceAsStream(path)) {
             byte[] buffer = new byte[10 * 1024];
 
